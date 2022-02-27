cfe6a3e3e084714df2af8ac174d11e4f4b998be4
Vidwat Kanade <vidwatk@gmail.com>
2022-02-27 02:53:47 +0000

    complete(SearchBar.js): Added the google search bar (No functionalities)

--- screens/Home.js
+++ screens/Home.js
@@ -1,10 +1,17 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import HeaderTabs from '../components/HeaderTabs'
+import SearchBar from '../components/SearchBar'
 
 export default function Home() {
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <SafeAreaView style= {{backgroundColor: '#bfbfbf', flex: 1}}>
+        <View style= {{ backgroundColor: 'white', padding: 15 }}>
+            <HeaderTabs/>
+            <SearchBar  />
+        </View>
+        <Categories/>
+    </SafeAreaView>
   )
 }
