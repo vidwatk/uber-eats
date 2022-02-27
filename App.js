315fb255173fee703eaad5fb2151ffe47e85498a
Vidwat Kanade <vidwatk@gmail.com>
2022-02-27 02:54:47 +0000

    complete(SearchBar.js): everything except functionalities

--- App.js
+++ App.js
@@ -5,9 +5,5 @@ import {SafeAreaView} from 'react-native-safe-area-context';
 
 
 export default function App() {
-  return(
-    <SafeAreaView>
-      <HeaderTabs />
-    </SafeAreaView>
-  );
+  return <Home/>
 }
