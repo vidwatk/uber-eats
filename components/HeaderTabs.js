c35b365642d3d74e733682342dc38fe8dbb7c221
Vidwat Kanade <vidwatk@gmail.com>
2022-02-27 23:08:48 +0000

    complete(Home.js): Added the set state functionality

--- components/HeaderTabs.js
+++ components/HeaderTabs.js
@@ -1,12 +1,11 @@
 import { View, Text, TouchableOpacity } from 'react-native'
 import React, {useState} from 'react'
 
-export default function HeaderTabs() {
-    const [activeTab, setActiveTab] = useState("Delivery")
+export default function HeaderTabs(props) {
   return (
     <View style= {{ flexDirection: "row", alignSelf: "center" }}>
-      <HeaderButton text="Delivery" btnColor="black" textColor="white" activeTab={activeTab} setActiveTab={setActiveTab} />
-      <HeaderButton text="Pick up" btnColor="white" textColor="black"  activeTab={activeTab} setActiveTab={setActiveTab} />
+      <HeaderButton text="Delivery" btnColor="black" textColor="white" activeTab={props.activeTab} setActiveTab={props.setActiveTab} />
+      <HeaderButton text="Pick up" btnColor="white" textColor="black"  activeTab={props.activeTab} setActiveTab={props.setActiveTab} />
       
     </View>
   );
--- screens/Home.js
+++ screens/Home.js
@@ -1,5 +1,5 @@
 import { View, Text, ScrollView } from 'react-native';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import HeaderTabs from '../components/HeaderTabs';
 import SearchBar from '../components/SearchBar';
@@ -11,8 +11,11 @@ const YELP_API_KEY = "Tq_QYeUvgXhbDiBQru5ja5AfBndqf0NnFOrTek5h1YvBHEFhW8fUOOj-Yt
 
 export default function Home() {
   const [restaurantData, setRestaurantData] = React.useState(localRestaurants)
+  const [city, setCity] = useState("New York")
+  const [activeTab, setActiveTab] = useState("Delivery")
+
   const getRestaurantsFromYelp = () => {
-    const yelpUrl= "https://api.yelp.com/v3/businesses/search?restaurants&location=Manchester"
+    const yelpUrl= `https://api.yelp.com/v3/businesses/search?restaurants&location=${city}`
 
 
   const apiOptions = {
@@ -24,18 +27,19 @@ export default function Home() {
 
     return fetch(yelpUrl, apiOptions)
     .then((res) => res.json())  
-    .then(json => setRestaurantData(json.businesses));
+    .then(json => setRestaurantData(json.businesses.filter((business) => business.transactions.includes(activeTab.toLowerCase()))));
   };
 
+  // getting the details from the api and rendering them on the screen
   useEffect(() => {
-    //getRestaurantsFromYelp();
-  }, [])
+    getRestaurantsFromYelp();
+  }, [city, activeTab])
 
   return (
     <SafeAreaView style= {{backgroundColor: '#bfbfbf', flex: 1}}>
         <View style= {{ backgroundColor: 'white', padding: 15 }}>
-            <HeaderTabs/>
-            <SearchBar  />
+            <HeaderTabs activeTab={activeTab} setActiveTab={setActiveTab}/>
+            <SearchBar cityHandler={setCity} />
         </View>
         <ScrollView showsVerticalScrollIndicator={false}>
         <Categories />
